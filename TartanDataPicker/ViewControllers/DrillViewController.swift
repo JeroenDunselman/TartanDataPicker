ece728db064b214174c0089c965e4102ed867c6b
Jeroen Dunselman <jeroduns@gmail.com>
2020-02-10 17:22:16 +0100

    Cleanup unused from project

--- TartanDataPicker/ViewControllers/DrillViewController.swift
+++ TartanDataPicker/ViewControllers/DrillViewController.swift
@@ -10,7 +10,6 @@ import Foundation
 import UIKit
 
 class DrillViewController: UITableViewController {
-    var drillVC: AssetViewController?
     func presentDetail(_ viewControllerToPresent: UIViewController) {
         let transition = CATransition()
         transition.duration = 0.25
@@ -25,16 +24,5 @@ class DrillViewController: UITableViewController {
     @objc func goBack(){
         dismiss(animated: true, completion: nil)
     }
-    func drillToAsset(_ asset: Asset){
-        if drillVC == nil {
-            drillVC = (storyboard!.instantiateViewController(withIdentifier: "AssetTVC") as! AssetViewController)}
-        
-        if let vc = drillVC {
-            vc.asset = asset //assets?.assets.reversed()[indexPath.row] //assets?.boards[indexPath.row]
-            vc.navigationItem.title = "hatseflats"
-            vc.navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem:UIBarButtonItem.SystemItem.done, target: self, action: #selector(goBack))
-            navController = UINavigationController(rootViewController: vc)
-            presentDetail(navController!)
-        }
-    }
+
 }
